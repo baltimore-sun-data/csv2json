02e6f2cb0a0442c3f09fc95a2c06cb2b8ea00cc3
Carl Johnson <me@carlmjohnson.net>
2018-02-06 10:39:06 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	enc, err := FromArgs(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Initial error: %v\n", err)
+		os.Exit(3)
+	}
+	if err = enc.Encode(); err != nil {
+		fmt.Fprintf(os.Stderr, "Run time error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func deferClose(err *error, f func() error) {
+	newErr := f()
+	if *err == nil {
+		*err = newErr
+	}
+}
+
+type Encoder struct {
+	src         io.ReadCloser
+	dest        io.WriteCloser
+	transformer func() error
+}
+
+func FromArgs(args []string) (*Encoder, error) {
+	fl := flag.NewFlagSet("csv2json", flag.ExitOnError)
+
+	src := fl.String("src", "", "Source file (default: stdin)")
+	dest := fl.String("dest", "", "Destination file (default: stdout)")
+	_ = fl.Parse(args)
+
+	e := Encoder{
+		src:  os.Stdin,
+		dest: os.Stdout,
+	}
+	if *src != "" && *src != "-" {
+		f, err := os.Open(*src)
+		if err != nil {
+			return nil, err
+		}
+		e.src = f
+	}
+	if *dest != "" && *dest != "-" {
+		f, err := os.Create(*dest)
+		if err != nil {
+			return nil, err
+		}
+		e.dest = f
+	}
+	e.transformer = e.WithHeaders
+	return &e, nil
+}
+
+func (e *Encoder) Encode() error {
+	return e.transformer()
+}
+
+func (e *Encoder) WithHeaders() (err error) {
+	defer deferClose(&err, e.dest.Close)
+
+	data, err := makeWithHeader(e.src)
+	if err != nil {
+		return err
+	}
+
+	enc := json.NewEncoder(e.dest)
+	return enc.Encode(&data)
+}
+
+func makeWithHeader(src io.ReadCloser) (data []map[string]string, err error) {
+	defer deferClose(&err, src.Close)
+
+	cr := csv.NewReader(src)
+	cr.Comment = '#'
+	cr.FieldsPerRecord = -1
+	cr.ReuseRecord = true
+
+	fields, err := cr.Read()
+
+	// Save headers for each row of dict
+	dataHeader := make(map[int]string, len(fields))
+	for i, field := range fields {
+		dataHeader[i] = field
+	}
+
+	for {
+		fields, err = cr.Read()
+		if err == io.EOF {
+			return data, nil
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		datum := make(map[string]string, len(fields))
+		for i, val := range fields {
+			datum[dataHeader[i]] = val
+		}
+		data = append(data, datum)
+	}
+}
